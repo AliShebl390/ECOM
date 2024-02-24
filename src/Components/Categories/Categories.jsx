ce969e252453772d2386f144f7e4cebdb36fb90c
Ali Shebl <80364389+AliShebl390@users.noreply.github.com>
2024-02-24 16:19:06 +0200

    Adding Categories Page

--- src/Components/Categories/Categories.jsx
+++ src/Components/Categories/Categories.jsx
@@ -6,12 +6,42 @@ export default function Categories() {
     function getGategories() {
         return axios.get("https://ecommerce.routemisr.com/api/v1/categories");
     }
-    useQuery(getGategories, {
-      
+    let { data, isLoading } = useQuery("Gategories", getGategories, {
+        cacheTime: 300000,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        refetchInterval: 1800000,
     });
+
+    function getSpecificCategory(id) {}
+
+    console.log(data);
     return (
-        <div className="container py-5 products-container mt-5 rounded-3">
-            <h1>Categories</h1>
+        <div className="container p-5 products-container mt-5 rounded-3">
+            <div className="row g-5">
+                {data?.data?.data?.map((category) => {
+                    return (
+                        <div
+                            onClick={(id) => getSpecificCategory(category._id)}
+                            className="cursor-pointer col-md-4 col-sm-6 col-lg-3"
+                        >
+                            <div className="card">
+                                <img
+                                    style={{ height: "340px" }}
+                                    src={category.image}
+                                    className="card-img-top"
+                                    alt="..."
+                                />
+                                <div className="card-body">
+                                    <h5 className="card-title text-center text-capitalize m-0 fw-bolder">
+                                        {category.name}
+                                    </h5>
+                                </div>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
 }
